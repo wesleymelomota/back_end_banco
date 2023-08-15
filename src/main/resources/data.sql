e4707df134f106e73329a5bea9dcfe50c30b9ac4
Wesley Melo mota <wesleymota382@gmail.com>
2023-08-14 22:14:56 -0300

    alterando saldo das contas fulano e sicrano

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -51,8 +51,8 @@ CREATE TABLE IF NOT EXISTS transaction (
 );
 
 
-INSERT INTO saldo (id, saldo) VALUES (1, 0.0);
-INSERT INTO saldo (id, saldo) VALUES (2, 0.0);
+INSERT INTO saldo (id, saldo) VALUES (1, 30907.70);
+INSERT INTO saldo (id, saldo) VALUES (2, 24672.73);
 INSERT INTO conta (id_conta, nome_responsavel, numero_conta, saldo_id) VALUES (1,'Fulano', 12345, 1);
 INSERT INTO conta (id_conta, nome_responsavel, numero_conta, saldo_id) VALUES (2,'Sicrano', 54321, 2);
 INSERT INTO usuario ( name, username, password, email, role, conta_id) VALUES ( 'Fulano', 'fulano', '$2a$10$/u.82nDXKL7xV6ZIal7CDOW5iZN9jgLAl3dg0h2vnWNddeMZog/K2', 'fulano@gmail.com', 'USER', 1);
@@ -64,7 +64,7 @@ INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_tra
 INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (4,'2019-08-07 ',-530.50,'SAQUE', null,2);
 INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (5,'2020-06-08 ',3241.23,'TRANSFERENCIA', 'Beltrano',1);
 INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (6,'2021-04-01 ',25173.09,'TRANSFERENCIA', 'Ronnyscley',2);
-INSERT INTO transaction (id, data_transacao, saldo, conta_id_conta, transferencia_id) VALUES (1, '2019-01-01 12:00:00+03', 500, 1, 1);
-INSERT INTO transaction (id, data_transacao, saldo, conta_id_conta, transferencia_id) VALUES (2, '2019-01-01 12:00:00+03', 12.24, 1, 2);
-INSERT INTO transaction (id, data_transacao, saldo, conta_id_conta, transferencia_id) VALUES (3, '2019-01-01 12:00:00+03', -500, 2, 3);
-INSERT INTO transaction (id, data_transacao, saldo, conta_id_conta, transferencia_id) VALUES (4, '2019-01-01 12:00:00+03', -530.50, 2, 4);
+INSERT INTO transaction (id, data_transacao, saldo, conta_id_conta, transferencia_id) VALUES (1, '2019-01-01 12:00:00+03', 30895.46, 1, 1);
+INSERT INTO transaction (id, data_transacao, saldo, conta_id_conta, transferencia_id) VALUES (2, '2019-01-03 12:00:00+03', 12.24, 1, 2);
+INSERT INTO transaction (id, data_transacao, saldo, conta_id_conta, transferencia_id) VALUES (3, '2019-01-05 12:00:00+03', -500.50, 2, 3);
+INSERT INTO transaction (id, data_transacao, saldo, conta_id_conta, transferencia_id) VALUES (4, '2019-01-07 12:00:00+03', 25173.23, 2, 6);
